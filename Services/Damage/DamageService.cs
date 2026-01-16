c36815f1a9891898c2dd9c5e65bd4f5283536a1b
lee <leesingeon@gmail.com>
2026-01-16 08:01:58 +0800

    fix serice

--- Services/Damage/DamageService.cs
+++ Services/Damage/DamageService.cs
@@ -63,11 +63,7 @@ namespace VRMS.Services.Damage
                 description,
                 estimatedCost);
         }
-        public List<(int DamageReportId, string Description, decimal EstimatedCost)>
-    GetApprovedDamagesByRental(int rentalId)
-        {
-            return _reportRepo.GetApprovedByRental(rentalId);
-        }
+
         public void UpdateDamage(
             int damageId,
             DamageType damageType,
